3d4d0731c9b28da04d32f35c3732a4d1361fb83f
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-20 16:04:06 -0300

    Program.cs

--- Proyecto - CSharp/Ignis/src/Program.cs
+++ Proyecto - CSharp/Ignis/src/Program.cs
@@ -10,6 +10,10 @@ namespace Ignis //src
 
             Console.WriteLine("{0} {1} {2} {3}", p1.Nombre, p1.Correo, p1.Contrasena, p1.Status);
 
+            Tecnico t1 = new Tecnico("Gonzalo", "gonza@correo", "abc123", 33);
+
+            Console.WriteLine("{0} {1} {2} {3} {4}", t1.Nombre, t1.Correo, t1.Contrasena, t1.Edad, t1.Status);
+
         }
     }
 }
