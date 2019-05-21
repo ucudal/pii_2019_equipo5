103cc3545a4bcbbe8551b95450f009c6ec313789
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-05-21 10:48:36 -0300

    Clase Persona

--- Proyecto - CSharp/Ignis/src/Persona.cs
+++ Proyecto - CSharp/Ignis/src/Persona.cs
@@ -9,8 +9,8 @@ namespace Ignis
         /// Considerando la propiedad polimórfica, implementamos herencia porque todas 
         /// estas clases utilizan todos sus atríbutos y comportamientos.
         /// 
-        /// A los efectos de encapsulamiento implementamos getters y setters, que además 
-        /// nos permitió validar el ingreso de valores.
+        /// A los efectos de encapsulamiento, implementamos getters y setters 
+        /// que nos permitió validar el ingreso de valores.
         /// </summary>
         public Persona(string Nombre, string Correo, string Contrasena) 
         {
@@ -20,6 +20,7 @@ namespace Ignis
             this.status = statusAux.Valor;
         }
 
+        private Status statusAux = new Status(true);
 
         /// <summary>
         /// Atributo: Nombre.
@@ -55,10 +56,13 @@ namespace Ignis
                 return this.correo; 
             }
             set 
-            {
-                        // agregar sentencias de control
+            { 
+                ValidarEmail ve = new ValidarEmail();
 
-                this.correo = value; 
+                if ( !ve.EsUnEmailValido(value) )
+                    throw new ArgumentException("El formato de correo ingresado no es correcto.");
+                
+                this.correo = value;
             }
         }
 
@@ -74,33 +78,13 @@ namespace Ignis
                 return this.contrasena;
             }
             set 
-            {
-                ValidarEmail ve = new ValidarEmail();
+            { 
+                ValidarContrasena vc= new ValidarContrasena();
+                
+                if ( !vc.EsUnaContrasenaValida(value) ) 
+                    throw new ArgumentException("El formato o largo de la contraseña no es válida.");
 
-                if ( string.IsNullOrWhiteSpace(value) )
-                    throw new ArgumentException("No se puede ingresar un valor nulo o vacío.");
-                    
-                if ( !ve.EsUnEmailValido(value) ) 
-                    throw new ArgumentException("NLa dirección indicada no tiene el formato correcto.");
-
-                this.CambiarContrasena(value);
-            }
-        }
-
-
-        /// <summary>
-        /// Método para el cambio de contraseña.
-        /// </summary> 
-        private void CambiarContrasena(string value) 
-        {
-            /// Controlamos que el valor ingresado no sea nula y que tenga un largo mínimo de cuatro carácteres.
-            if ( (string.IsNullOrEmpty(value)) && (value.Length < 4) ) 
-            {
-                Console.WriteLine("Se ingresó un valor que no cumple con los requisitos. Debe tener un largo mayor a 4 carácteres, no puede ser nulo o vacío.");
-            }
-            else 
-            {
-                this.contrasena = value; 
+                this.contrasena = value;
             }
         }
 
@@ -114,8 +98,5 @@ namespace Ignis
             get => status;
             set => this.status = value;
         }
-
-        private Status statusAux = new Status(true);
-
     }
 }
