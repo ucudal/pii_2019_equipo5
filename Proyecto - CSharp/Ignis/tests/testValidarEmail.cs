a97df21069dd8c4df3c3a3af573537d586ef1467
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-20 22:57:35 -0300

    Test ValidarEmail

---
+++ Proyecto - CSharp/Ignis/tests/testValidarEmail.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+using Ignis;
+
+namespace tests
+{
+    public class testsValidarEmail 
+    { 
+        /// <summary> 
+        /// Testeamos que la clase ValidarEmail realiza el comportamiento esperado.
+        /// </summary>
+
+
+        [Fact]
+        public void email_correcto_debe_retornar_true()
+        {
+            // preparación
+            string emailIngresado = "equipo@ucu.edu.uy";
+            ValidarEmail ve = new ValidarEmail();
+            bool actual = ve.EsUnEmailValido(emailIngresado);
+
+            // esperado
+            bool expected = true;
+
+            // test
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void email_incorrecto_debe_retornar_false()
+        {
+            // preparación
+            string emailIngresado = "equipo.ucu.edu.uy";
+            ValidarEmail ve = new ValidarEmail();
+            bool actual = ve.EsUnEmailValido(emailIngresado);
+
+            // esperado
+            bool expected = false;
+
+            // test
+            Assert.Equal(expected, actual);
+        }
+    }
+}
