bb972ca18b341bec63e58c4e038f33ef770bd975
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-05-21 10:48:48 -0300

    Clase ValidarContrasena

---
+++ Proyecto - CSharp/Ignis/src/ValidarContrasena.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Ignis 
+{   
+    public class ValidarContrasena  
+    { 
+        /// <summary>
+        /// Método para controlar que la contraseña ingresada tiene un formato válido.
+        /// Para este caso, controlamos que no sea 'vacío' que no sea 'null'
+        /// y que el largo sea mayor a 4 carácteres.
+        /// Si es necesario, se pueden agregar otras condiciones aquí.
+        /// </summary> 
+        public bool EsUnaContrasenaValida(string value) 
+        {
+            if ( (string.IsNullOrEmpty(value)) || (value.Length < 4) ) 
+            {
+                return false;
+            }
+            else 
+            {
+                return true; 
+            }
+        }
+    }
+}
