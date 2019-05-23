4993b4dce91e35b7024fe2f749c0fbfec0dcc6bf
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-22 23:34:56 -0300

    Clase Program

--- Proyecto - CSharp/Ignis/src/Program.cs
+++ Proyecto - CSharp/Ignis/src/Program.cs
@@ -6,13 +6,38 @@ namespace Ignis //src
     {
         static void Main(string[] args)
         {
-            Persona p1 = new Persona("Marcelo", "marce@correo", "qwer1234");
+            // probando la creación de una persona
+            Persona pp = new Persona("Marcelo", "marce@ucu.uy", "qwer1234");
 
-            Console.WriteLine("{0} {1} {2} {3}", p1.Nombre, p1.Correo, p1.Contrasena, p1.Status);
+            Console.WriteLine("{0} {1} {2} {3}", pp.Nombre, pp.Correo, pp.Contrasena, pp.Status);
 
-            Tecnico t1 = new Tecnico("Gonzalo", "gonza@correo", "abc123", 33);
+            // probando la creación de un técnico.
+            Tecnico tt = new Tecnico("Gonzalo", "gonza@ucu.uy", "abc123", 33);
 
-            Console.WriteLine("{0} {1} {2} {3} {4}", t1.Nombre, t1.Correo, t1.Contrasena, t1.Edad, t1.Status);
+            Console.WriteLine("{0} {1} {2} {3} {4}", tt.Nombre, tt.Correo, tt.Contrasena, tt.Edad, tt.Status);
+
+            // probando validar un email.
+            ValidarEmail ve = new ValidarEmail();
+
+            string emaildir1 = "correoCorrecto@dominio.com";
+
+            Console.WriteLine("Validar " + emaildir1 + "nos devuelve: " + ve.EsUnEmailValido(emaildir1));
+
+            string emaildir2 = "correoIncorrecto@dominio";
+
+            Console.WriteLine("Validar " + emaildir2 + "nos devuelve: " + ve.EsUnEmailValido(emaildir2));
+
+            // probando validar una contraseña.
+            ValidarContrasena vc = new ValidarContrasena();
+
+            string clave1 = "";
+            string clave2 = "a@1";
+            string clave3 = "ClaveCorrecta1";
+
+            Console.WriteLine("Validar la contraseña: " + clave1 + "(vacío) nos devuelve: " + vc.EsUnaContrasenaValida(clave1));
+            Console.WriteLine("Validar la contraseña: " + clave2 + " nos devuelve: " + vc.EsUnaContrasenaValida(clave2));
+            Console.WriteLine("Validar la contraseña: " + clave3 + " nos devuelve: " + vc.EsUnaContrasenaValida(clave3));
+
 
         }
     }
