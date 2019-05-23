5ce5b9dea904b4d51622c65217b5cca90f8b08af
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-05-23 11:43:35 -0300

    testTecnico

--- Proyecto - CSharp/Ignis/tests/testTecnico.cs
+++ Proyecto - CSharp/Ignis/tests/testTecnico.cs
@@ -12,15 +12,12 @@ namespace tests
         [Fact]
         public void construir_un_objeto_tecnico()
         {
-            // preparación
             Tecnico t1 = new Tecnico("Gonzalo", "gonza@correo", "abc123", 33);
 
             string actual = string.Format(@"{0} {1} {2} {3} {4}", t1.Nombre, t1.Correo, t1.Contrasena, t1.Edad, t1.Status);
 
-            // esperado
             string expected = "Gonzalo gonza@correo abc123 33 True";
 
-            // test
             Assert.Equal(expected, actual);
         }
     }
