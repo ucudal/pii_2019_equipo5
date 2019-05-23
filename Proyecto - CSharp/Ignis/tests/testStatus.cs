d042ab855ea19c4e2d403402be55c2b9266baf82
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-22 23:36:04 -0300

    Clase Status

--- Proyecto - CSharp/Ignis/tests/testStatus.cs
+++ Proyecto - CSharp/Ignis/tests/testStatus.cs
@@ -12,14 +12,11 @@ namespace tests
         [Fact]
         public void construir_un_objeto_status_con_valor_true()
         {
-            // preparación.
             Status s1 = new Status(true);
             bool actual = s1.Valor;
 
-            // esperado.
             bool expected = true;
 
-            // test
             Assert.Equal(expected.ToString(), actual.ToString());
         }
 
@@ -30,15 +27,12 @@ namespace tests
         [Fact]
         public void cambiar_status_de_usuario_mediante_metodo_inactivar()
         {
-            // preparación.
             Status s1 = new Status(true);   // creo el objeto con valor true.
             s1.Inactivar();                 // cambio el valor mediante el método.
             bool actual = s1.Valor;
 
-            // esperado.
             bool expected = false;
 
-            // test
             Assert.Equal(expected.ToString(), actual.ToString());
         }
 
@@ -49,15 +43,12 @@ namespace tests
         [Fact]
         public void cambiar_status_de_usuario_mediante_metodo_activar()
         {
-            // preparación.
             Status s1 = new Status(false);   // creo el objeto con valor false.
             s1.Activar();                    // cambio el valor mediante el método.
             bool actual = s1.Valor;
 
-            // esperado.
             bool expected = true;
 
-            // test
             Assert.Equal(expected.ToString(), actual.ToString());
         }
     }
