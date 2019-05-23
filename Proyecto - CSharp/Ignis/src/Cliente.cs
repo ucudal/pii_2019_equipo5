da82f3999ed00de08a83d02869b561183203dfdf
Sofia Nicoletti <sofia-nico@hotmail.com>
2019-05-23 17:54:15 -0300

    Create Cliente.cs

---
+++ Proyecto - CSharp/Ignis/src/Cliente.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Ignis
+{
+    public class Cliente : Persona
+    {
+        /// <summary>
+        /// El Cliente es la persona que se registra en la aplicación para postular sus proyectos, también podrá dar de alta una o más solicitudes de técnicos.
+        
+        /// La clase Cliente hereda todos los atributos y comportamientos de la clase Persona y de la clase Proyecto.
+        /// </summary>
+        public Cliente (string Nombre, string Correo, string Contrasena, Int32 Edad, 
+                        string presentacion, string condAcademica, int anioEgreso, 
+                        string nivel_experiencia, int calificacionClientes, int calificacionIgnis) 
+                : base(Nombre, Correo, Contrasena) 
+    }
+}
