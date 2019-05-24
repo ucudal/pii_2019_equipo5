04f9595ca5123babf4aa4f12fdf3ffd01689e354
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-05-24 15:42:57 -0300

    Clase Tecnico

--- Proyecto - CSharp/Ignis/src/Tecnico.cs
+++ Proyecto - CSharp/Ignis/src/Tecnico.cs
@@ -11,49 +11,103 @@ namespace Ignis
         /// La clase Tecnico hereda todos los atributos y comportamientos de la clase Persona.
         /// </summary>
         public Tecnico(string Nombre, string Correo, string Contrasena, Int32 Edad, 
-                        string presentacion, string condAcademica, int anioEgreso, 
-                        string nivel_experiencia, int calificacionClientes, int calificacionIgnis) 
-                : base(Nombre, Correo, Contrasena) 
-        {
+                        string presentacion, string nivel_experiencia, 
+                        int calif_Clientes, int calif_Ignis) 
+                    : base(Nombre, Correo, Contrasena) 
+        { 
+            Check.Precondicion(!string.IsNullOrEmpty(Edad.ToString()), "La edad no puede ser nulo o vacío.");
+            Check.Precondicion((Edad > 0), "La edad debe ser mayor que cero.");
+
             this.edad = Edad;
             this.presentacion = "";         // Texto de presentación de sí mismo.
-            this.condAcademica = "";        // 'Estudiante', 'Egresado'.
-            this.anioEgreso = 0;
             this.nivel_experiencia = "";    // 'Básico', 'Avanzado'.
-            this.calificacionClientes = 0;  // Rango del 0 al 5.
-            this.calificacionIgnis = 0;     // Rango del 0 al 5.
+            this.calif_Clientes = 0;        // Rango del 0 al 5.
+            this.calif_Ignis = 0;           // Rango del 0 al 5.
         }
 
-        
         /// <summary>
-        /// Atributo: Edad. 
-        /// Validación: la edad debe ser mayor a cero, no puede ser nulo o vacío. 
-        /// Este parámetro puede ser cambiado por el Administrador, ya que podria especificar una edad 
-        /// mínima de contratación de técnicos (ejem. 18 años).
+        /// Edad. 
+        /// 
+        /// La edad debe ser mayor a cero, no puede ser nulo o vacío. 
         /// </summary>
         private Int32 edad;
         public Int32 Edad 
         {
-            get { return edad; }
+            get 
+            { 
+                return edad; 
+            }
             set 
             { 
-                if ( string.IsNullOrEmpty(value.ToString()) && (value > 0) ) 
-                {
-                    Console.WriteLine("Error. No puede ingresar un valor nulo o vacío. El valor debe ser mayor a cero.");
-                }
-                else 
-                {
-                    this.edad = value;
-                }
+                Check.Precondicion(!string.IsNullOrEmpty(Edad.ToString()), "La edad no puede ser nulo o vacío.");
+                Check.Precondicion((Edad > 0), "La edad debe ser mayor que cero.");
+
+                this.edad = value;
+
+                Check.Postcondicion(this.Edad == value, "Edad no fue actualizado.");
             }
         }
 
+        /// <summary>
+        /// Presentación
+        /// 
+        /// El técnico puede incluir un texto para aclarar cosas, describir su forma de trabajo o intéreses.
+        /// </summary>
         private string presentacion { get; set; }
-        private string condAcademica { get; set; }
-        private int anioEgreso { get; set; } 
+        public string  Presentacion
+        {
+            get => this.presentacion;
+            set => this.presentacion = Presentacion;
+        }
+
+        /// <summary>
+        /// Nivel de Experiencia
+        /// 
+        /// Nivel que el técnico se adjudica de experiencia.
+        /// </summary>
+        /// <value>"Básico", "Avanzado"</value>
         private string nivel_experiencia { get; set; }
-        private int calificacionClientes { get; set; }
-        private int calificacionIgnis { get; set; }
+        public string  Nivel_experiencia
+        {
+            get => this.nivel_experiencia;
+            set => this.nivel_experiencia = Nivel_experiencia;
+        }
+
+        /// <summary>
+        /// Calificación promedio de los clientes.
+        /// </summary>
+        /// <value>Rango: 0 al 5, siendo 5 la mejor calificación.</value>
+        private int calif_Clientes { get; set; }
+        public int  Calif_Clientes
+        {
+            get => this.calif_Clientes;
+            set => this.calif_Clientes = value;
+        }
+
+        /// <summary>
+        /// Calificación del Centro Ignis.
+        /// </summary>
+        /// <value>Rango: 0 al 5, siendo 5 la mejor calificación.</value>
+        private int calif_Ignis { get; set; }
+        public int Calif_Ignis
+        {
+            get => this.calif_Ignis;
+            set => this.calif_Ignis = value;
+        }
+
+        // /// <summary>
+        // /// Método para modificar la calificación de clientes.
+        // /// 
+        // /// Esta calificación es un promedio, ya que se considera la calificación 
+        // /// que el técnico tenía de otros usuarios antes de agregarse una nueva.
+        // /// </summary>
+        // /// <value>Rango: 0 al 5, siendo 5 la mejor calificación.</value>
+        // public void modificarCalif_Clientes(int nuevaCalificacion) 
+        // {
+        //     Check.Precondicion((nuevaCalificacion >= 0 && nuevaCalificacion <= 5) , "El rango debe estar entre 0 y 5.");
+
+        //     if (nuevaCalificacion == 0) this.calificacionClientes == 
+        // }
 
     }
 }
