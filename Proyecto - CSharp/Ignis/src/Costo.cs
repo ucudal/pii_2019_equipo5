20b71040b6bf643579fcc8e3213ce06904939064
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-30 14:27:03 -0300

    Clase Costo

--- Proyecto - CSharp/Ignis/src/Costo.cs
+++ Proyecto - CSharp/Ignis/src/Costo.cs
@@ -1,14 +1,38 @@
 using System;
 
-namespace Ignis
-{
-    class Costo
+namespace Ignis 
+{   
+    public class Costo 
     {
-        
-        public CalcularCosto(ListaCostos,PrecioPorHora)
+        public Costo() 
         {
+            this.costoHoraBasico = 0;
+            this.costoHoraAvanzado = 0;
         }
 
+        private int costoHoraBasico;
+        public int CostoHoraBasico 
+        {
+            get => this.costoHoraBasico;
+            protected set {}
+        }
+
+        public void ModificarCostoHoraBasico(int nuevoCosto) 
+        {
+            this.costoHoraBasico = nuevoCosto;
+        }
+
+        private int costoHoraAvanzado;
+        public int CostoHoraAvanzado 
+        {
+            get => this.costoHoraAvanzado;
+            protected set {}
+        }
+
+        public void ModificarCostoHoraAvanzado(int nuevoCosto) 
+        {
+            this.costoHoraAvanzado = nuevoCosto;
+        }
 
     }
 }
