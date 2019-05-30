86d3451a8fc9d8c7c7fda22274cc384aa2742033
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-05-30 14:26:56 -0300

    Clase Cliente

--- Proyecto - CSharp/Ignis/src/Cliente.cs
+++ Proyecto - CSharp/Ignis/src/Cliente.cs
@@ -1,21 +1,14 @@
 using System;
 
 namespace Ignis
-{
-    public class Cliente : Persona
+{   
+    public class Cliente : Persona 
     {
-        /// <summary>
-        /// El Cliente es la persona que se registra en la aplicación para postular sus proyectos, también podrá dar de alta una o más solicitudes de técnicos.
-        
-        /// La clase Cliente hereda todos los atributos y comportamientos de la clase Persona y de la clase Proyecto.
-        /// </summary>
-        public Cliente (string Nombre, string Correo, string Contrasena) 
-                : base(Nombre, Correo, Contrasena)
-                {
-                    
-                }
-        public desactivarCliente (){
-            
+        public Cliente(string Nombre, string Correo, string Contrasena) 
+                    : base(Nombre, Correo, Contrasena) 
+        { 
+            // Nombre, correo y contraseña: se chequea precondiciones en la clase Persona.
         }
+
     }
 }
