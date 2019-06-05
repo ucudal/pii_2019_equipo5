a0250563cf73e5cec7b6b815a6823f09411c22ff
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-06-05 15:55:21 -0300

    Clases validaEmail y validaContrasena + interfases
    Se implementan interfases.

---
+++ RazorPagesIgnis/Models/IValidarContrasena.cs
@@ -0,0 +1,10 @@
+namespace RazorPagesIgnis 
+{
+    /// <summary>
+    /// Implementada por la clase ValidarContrasena.
+    /// </summary>
+    public interface IValidarContrasena 
+    {
+        bool EsUnaContrasenaValida(string valor);
+    }
+}
