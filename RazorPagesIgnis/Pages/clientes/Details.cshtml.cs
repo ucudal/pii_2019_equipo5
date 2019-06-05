0457d6177019cf76154a267759dc20dcd02fef9d
Marcelo Stabile <42213352+marcelostabile@users.noreply.github.com>
2019-06-05 13:49:00 -0300

    razorpages pages

--- RazorPagesIgnis/Pages/clientes/Details.cshtml.cs
+++ RazorPagesIgnis/Pages/clientes/Details.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Ignis;
+using RazorPagesIgnis;
 using RazorPagesIgnis.Models;
 
 namespace RazorPagesIgnis.Pages.clientes
