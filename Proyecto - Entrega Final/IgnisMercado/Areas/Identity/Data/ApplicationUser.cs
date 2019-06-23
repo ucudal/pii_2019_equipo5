a63e2b7a046b710b34d6a6ff265281ccad261ae3
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-06-22 23:12:53 -0300

    AppUser status

--- Proyecto - Entrega Final/IgnisMercado/Areas/Identity/Data/ApplicationUser.cs
+++ Proyecto - Entrega Final/IgnisMercado/Areas/Identity/Data/ApplicationUser.cs
@@ -1,26 +1,31 @@
 using System;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
-
 using IgnisMercado.Models;
 
 namespace IgnisMercado.Areas.Identity.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
+    // Add profile data for application users by adding properties to the ApplicationUser class.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         [PersonalData]
+        [StringLength(100)]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
 
-        [Display(Name = "Date of Birth")]
-        [DataType(DataType.Date)]
+        [Required]
         [PersonalData]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
         public DateTime DOB { get; set; }
 
         // Es necesario tener acceso a IdentityManager para poder buscar el rol de este usuario; esta propiedad se asigna cada vez que se
         // cambia el rol usando IdentityManager para poder buscar por rol después cuando no hay acceso a IdentityManager. La propiedad
         // puede ser null para los usuarios que todavía no tienen un rol asignado.
 
+        [Required]
+        [Display(Name = "Perfil")]
         public string Role { get; private set; }
 
         // El IdentityManager que se recibe como argumento no se usa en el método; sólo fuera a que el rol del usuario sea cambiado cuando
@@ -32,62 +37,26 @@ namespace IgnisMercado.Areas.Identity.Data
             {
                 throw new ArgumentNullException("identityManager");
             }
-
             this.Role = role;
         }
 
-        // /// <summary>
-        // /// Nivel de Experiencia
-        // /// 
-        // /// Nivel que el técnico se adjudica de experiencia.
-        // /// </summary>
-        // /// <value>"Básico", "Avanzado"</value>
-        // private string NivelExperiencia;
-        // public string nivelExperiencia
-        // {
-        //     get { return this.NivelExperiencia; }
-        //     set { 
-        //         Check.Precondicion((value == "Básico" || value == "Avanzado"), "Nivel de experiencia no admitido.");
-
-        //         this.NivelExperiencia = value;
-
-        //         Check.Postcondicion((value == "Básico" || value == "Avanzado"), "Nivel de experiencia no fue actualizado.");
-        //         }
-        // }
+        /// <summary>
+        /// El Status permite al Administrador de Ignis habilitar/deshabilitar las operaciones de un usuario.
+        /// </summary>
+        [Display(Name = "Status")]
+        public bool Status { get; private set; }
 
         /// <summary>
-        /// El Status de un usuario permite al Administrador de Ignis habilitar/deshabilitar 
-        /// las operaciones en la aplicación. Por ejemplo, un técnico con Status = Inactivo 
-        /// puede ingresar y consultar históricos pero no puede ser asignado a proyectos.
-        /// 
-        /// Durante la creación del objeto Cliente, Tecnico o Administrador, Status se inicializa con valor 'Activo'.
+        /// Métodos para cambiar el status.
         /// </summary>
-        /// <returns>True = usuario activo; False = usuario inactivo</returns>
-        private bool status;
-        public bool Status 
+        public void StatusHabilitar() 
         {
-            get => this.status; 
-            protected set {}
+            this.Status = true;
         }
 
-        /// <summary>
-        /// Métodos para cambiar el status, 
-        /// Activar(): si el usuario está 'Inactivo' se cambia para 'Activo'.
-        /// Inactivar(): si el usuario está 'Activo' se cambia para 'Inactivo'.
-        /// </summary>
-        public void Activar() 
+        public void StatusInhabilitar() 
         {
-            if (this.status == false) this.CambiarStatus();
-        }
-
-        public void Inactivar() 
-        {
-            if (this.status == true) this.CambiarStatus();
-        }
-
-        private void CambiarStatus() 
-        {
-            this.status = !this.status;
+            this.Status = false;
         }
 
     }
