14c5da391bda0df3c4ec9df3bf4d210b6ac4338f
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-06-26 00:27:30 -0300

    seeding, relación en appcontext

--- Proyecto - Entrega Final/IgnisMercado/Models/ApplicationContext.cs
+++ Proyecto - Entrega Final/IgnisMercado/Models/ApplicationContext.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 using IgnisMercado.Areas.Identity.Data;
+using IgnisMercado.Models;
 
 namespace IgnisMercado.Models
 {
@@ -19,19 +20,75 @@ namespace IgnisMercado.Models
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
 
-            builder.Entity<Proyecto>()
-                .HasOne(p => p.Cliente)
-                .WithMany(c => c.ListaProyectos);
+            builder.Entity<RelacionClienteProyecto>().ToTable("RelacionClienteProyectos");
+            builder.Entity<RelacionProyectoSolicitud>().ToTable("RelacionProyectoSolicitudes");
+            builder.Entity<RelacionTecnicoRol>().ToTable("RelacionTecnicoRoles");
+            builder.Entity<Solicitud>().ToTable("Solicitudes");
+            builder.Entity<Proyecto>().ToTable("Proyectos");
+            builder.Entity<Rol>().ToTable("Roles");
+            builder.Entity<Costo>().ToTable("Costos");
 
-            builder.Entity<Solicitud>()
-                .HasOne(s => s.Proyecto)
-                .WithMany(p => p.ListaSolicitudes);
+            // Relación Cliente:Proyecto
+            builder.Entity<RelacionClienteProyecto>()
+                .HasKey(cp => new { cp.ClienteId, cp.ProyectoId });
 
-            builder.Entity<Solicitud>()
-                .HasOne(s => s.Tecnico)
-                .WithMany(t => t.ListaSolicitudes);
+            builder.Entity<RelacionClienteProyecto>()
+                .HasOne(cp => cp.Cliente)
+                .WithMany(c => c.RelacionClienteProyecto)
+                .HasForeignKey(cp => cp.ClienteId);
+
+            builder.Entity<RelacionClienteProyecto>()
+                .HasOne(cp => cp.Proyecto)
+                .WithMany(p => p.RelacionClienteProyecto)
+                .HasForeignKey(cp => cp.ProyectoId);
+
+            // Relación Proyecto:Solicitud
+            builder.Entity<RelacionProyectoSolicitud>()
+                .HasKey(ps => new { ps.ProyectoId, ps.SolicitudId });
+
+            builder.Entity<RelacionProyectoSolicitud>()
+                .HasOne(ps => ps.Proyecto)
+                .WithMany(p => p.RelacionProyectoSolicitud)
+                .HasForeignKey(ps => ps.ProyectoId);
+
+            builder.Entity<RelacionProyectoSolicitud>()
+                .HasOne(ps => ps.Solicitud)
+                .WithMany(s => s.RelacionProyectoSolicitud)
+                .HasForeignKey(ps => ps.SolicitudId);
+
+            // Relación Tecnico:Rol
+            builder.Entity<RelacionTecnicoRol>()
+                .HasKey(tr => new { tr.TecnicoId, tr.RolId });
+
+            builder.Entity<RelacionTecnicoRol>()
+                .HasOne(tr => tr.Tecnico)
+                .WithMany(p => p.RelacionTecnicoRoles)
+                .HasForeignKey(tr => tr.TecnicoId);
+
+            builder.Entity<RelacionTecnicoRol>()
+                .HasOne(tr => tr.Rol)
+                .WithMany(s => s.RelacionTecnicoRoles)
+                .HasForeignKey(tr => tr.RolId);
+
+            // builder.Entity<Proyecto>()
+            //     .HasOne(p => p.Cliente)
+            //     .WithMany(c => c.ListaProyectos);
+
+            // builder.Entity<Solicitud>()
+            //     .HasOne(s => s.Proyecto)
+            //     .WithMany(p => p.ListaSolicitudes);
+
+            // builder.Entity<Solicitud>()
+            //     .HasOne(s => s.Tecnico)
+            //     .WithMany(t => t.ListaSolicitudes);
 
         }
+
+        public DbSet<IgnisMercado.Models.RelacionClienteProyecto> RelacionClienteProyectos { get; set; }
+
+        public DbSet<IgnisMercado.Models.RelacionProyectoSolicitud> RelacionProyectoSolicitudes { get; set; }
+
+        public DbSet<IgnisMercado.Models.RelacionTecnicoRol> RelacionTecnicoRoles { get; set; }
 
         public DbSet<IgnisMercado.Models.Administrador> Administradores { get; set; }
 
@@ -44,5 +101,8 @@ namespace IgnisMercado.Models
         public DbSet<IgnisMercado.Models.Proyecto> Proyectos { get; set; }
 
         public new DbSet<IgnisMercado.Models.Rol> Roles { get; set; }
+
+        public DbSet<IgnisMercado.Models.Costo> Costos { get; set; }
+
     }
 }
