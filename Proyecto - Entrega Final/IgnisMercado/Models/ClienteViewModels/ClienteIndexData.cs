e489a13a7548f73bf699b2707a9cb1a4761e9c3b
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-06-27 01:40:26 -0300

    Agrego select en página

--- Proyecto - Entrega Final/IgnisMercado/Models/ClienteViewModels/ClienteIndexData.cs
+++ Proyecto - Entrega Final/IgnisMercado/Models/ClienteViewModels/ClienteIndexData.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
+using IgnisMercado.Areas.Identity.Data;
 
 namespace IgnisMercado.Models.ClienteViewModels
 {
     public class ClienteIndexData
     {
-        public IEnumerable<Cliente> Clientes { get; set; }
+        public IList<ApplicationUser> Usuarios { get; set; }
 
-        public IEnumerable<Proyecto> Proyectos { get; set; }
+        public IList<Proyecto> Proyectos { get; set; }
 
-        public IEnumerable<Solicitud> Solicitudes { get; set; }
+        public IList<Solicitud> Solicitudes { get; set; }
 
     }
 }
--- Proyecto - Entrega Final/IgnisMercado/Pages/Usuarios/Index.cshtml.cs
+++ Proyecto - Entrega Final/IgnisMercado/Pages/Usuarios/Index.cshtml.cs
@@ -2,12 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using IgnisMercado.Areas.Identity.Data;
+
 using IgnisMercado.Models;
-using Microsoft.AspNetCore.Http;
+using IgnisMercado.Models.ClienteViewModels;
 
 namespace IgnisMercado.Pages.Usuarios
 {
@@ -20,15 +19,47 @@ namespace IgnisMercado.Pages.Usuarios
             _context = context;
         }
 
-        public IList<ApplicationUser> ApplicationUser { get;set; }
+        public string ClienteId { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            ApplicationUser = await _context.Users
-                                .OrderBy(u => u.Name)
-                                .OrderBy(u => u.Role)
+        public int ProyectoId { get; set; }
+
+        public ClienteIndexData ClienteIdxData  { get; set; }
+
+        //private ApplicationUser Usuario { get; set; }
+
+        //public IList<ApplicationUser> Usuarios { get;set; }
+
+        public IList<Proyecto> Proyectos { get; set; }
+
+        public IList<Proyecto> Proyectos2 { get; set; }
+
+        public async Task OnGetAsync(string id)
+        { 
+            ClienteIdxData = new ClienteIndexData();
+
+            // Mostrar los usuarios.
+            ClienteIdxData.Usuarios = await _context.Users
+                .OrderBy(u => u.Name)
+                .OrderBy(u => u.Role)
+                    .ToListAsync();
+
+            // Seleccinar un usuario.
+            if (id != null)
+            {
+                ClienteIdxData.Proyectos = await _context.Proyectos
+                        //.Include(p => p.RelacionClienteProyecto.Where(r => r.ClienteId == id))
+                            .OrderBy(p => p.Nombre)
                                 .ToListAsync();
-        }
+            };
 
+        }
     }
 }
+
+//     // ClienteIdxData.Clientes = await _context.Users
+//     //         .Include(c => c.RelacionClienteProyecto.Select(r => r.ClienteId == appUser.Id))
+//     //             .ThenInclude(p => p.Proyecto)
+//     //                 .OrderBy(u => u.Name)
+//     //                 .OrderBy(u => u.Role)
+//     //                     .AsNoTracking()
+//     //                     .ToListAsync();
