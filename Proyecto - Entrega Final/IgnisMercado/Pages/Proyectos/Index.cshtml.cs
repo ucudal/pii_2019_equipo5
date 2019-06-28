dce35c99235d1d40ef0dfe78e8f8bc5fa51d0c4b
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-06-27 23:48:35 -0300

    listas desplegables Proyectos

--- Proyecto - Entrega Final/IgnisMercado/Pages/Proyectos/Index.cshtml.cs
+++ Proyecto - Entrega Final/IgnisMercado/Pages/Proyectos/Index.cshtml.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+
 using IgnisMercado.Models;
+using IgnisMercado.Areas.Identity.Data;
+using IgnisMercado.Models.ClienteViewModels;
 
 namespace IgnisMercado.Pages.Proyectos
 {
@@ -18,17 +21,37 @@ namespace IgnisMercado.Pages.Proyectos
             _context = context;
         }
 
-        public IList<Proyecto> Proyecto { get;set; }
+        public ClienteIndexData ClienteIdxData  { get; set; }
 
-        public IList<Cliente> Cliente { get;set; }
+        //public IList<Cliente> Cliente { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string id)
         {
-            Proyecto = await _context.Proyectos.ToListAsync();
+            ClienteIdxData = new ClienteIndexData();
 
-            Cliente = await _context.Clientes.ToListAsync();
+            // Seleccionar un usuario, mostrar proyectos.
+            ClienteIdxData.Proyectos = await _context.Proyectos
+                .OrderBy(p => p.Nombre)
+                    .ToListAsync();
 
-            var Cli = from c in Cliente select c; 
+            // Seleccionar un proyecto, mostrar solicitudes.
+            if (id != null)
+            {
+                ClienteIdxData.Solicitudes = await _context.Solicitudes
+                        //.Include(p => p.RelacionClienteProyecto.Where(r => r.ClienteId == id))
+                    .OrderBy(s => s.RolRequerido)
+                    .OrderByDescending(s => s.NivelExperiencia)
+                        .ToListAsync();
+            };
+
+            // ClienteIdxData.Usuarios = await _context.Users
+            //     .OrderBy(u => u.Name)
+            //     .OrderBy(u => u.Role)
+            //         .ToListAsync();
+
+            // var Cli = from c in ClienteIdxData.Usuarios 
+            //     select c;
+
         }
     }
 }
